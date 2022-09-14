5de3b482cc883ac8197f5a3ace8fb44b2e042ba2
Chase Ridenour <cridenour@tgen.org>
2022-09-14 00:50:02 -0400

    Getting helper functions outlined for import_reads

--- q2_pan_classifier/actions/prep_sequences.py
+++ q2_pan_classifier/actions/prep_sequences.py
@@ -1,23 +1,63 @@
+import os
+import re
+import tempfile
 from pathlib import Path
-import gzip
-from q2_types.per_sample_sequences import SingleLanePerSamplePairedEndFastqDirFmt
+import qiime2
+from q2_types.per_sample_sequences import SingleLanePerSamplePairedEndFastqDirFmt, PairedEndSequencesWithQuality
+from q2_types.sample_data import SampleData
 
+def _get_forward_reverse_(name: str, file_names: list) -> tuple:
+    pass
+def _return_manifest_sample_string_(file_path_names: str) -> str:
+    #TODO
+    # 1. Change name from _return_names_
+    # 2. error handle regex mismatched name
+    # 3. finish _get_forward_reverse_ function, this will also check if name is unique
+    # 4. Have function return string for all samples in manifest
+    #
+
+    names_raw = [os.path.basename(x) for x in file_path_names]
+
+    names_out = list()
+
+    for full_name in names_raw:
+        try:
+            name = re.search(r'(.+)_.+_L[0-9][0-9][0-9]_R[12]_.+\.fastq.*', full_name).group(1)
+            names_out.append(name)
+        except AttributeError:
+            print(f"{full_name} did not match fastq naming scheme")
+    return names_out
+
+def _generate_manifest_file_(sample_dir_path: str, manifest_file_dir: str) -> str:
+    """Generates a manifest file in a temporary directory to be used in sequence reads upload"""
+
+    HEADER = ['sample-id', 'forward-absolute-filepath', 'reverse-absolute-filepath']
+
+    #TODO
+
+    sample_manifest_string = _return_manifest_sample_string_(sample_dir_path)
+
+    with open(os.path.join(manifest_file_dir, "manifest"), "w") as manifest_file:
+
+        manifest_file.write('\t'.join(HEADER) + '\n')
+        #TODO Write output from  _return_manifest_sample_string_
+        #manifest_file.write(sample_manifest_string)
+
+    return os.path.join(manifest_file_dir)
 
 def import_reads(sequence_directory: str) -> SingleLanePerSamplePairedEndFastqDirFmt:
-    output_directory = SingleLanePerSamplePairedEndFastqDirFmt()
 
-    with gzip.open(Path(output_directory.path).joinpath("a_a_L111_R1_001.fastq.gz"), "wb") as ff:
-        with open("/home/cridenour/PycharmProjects/q2-pan-classifier/test_data/ENTV_05Mar2022/trimmed_sequences/ENTV87Skyview20220228_S29_L001_R1_output.fastq", "rb") as fastq_file:
-            ff.writelines(fastq_file)
-    with gzip.open(Path(output_directory.path).joinpath("a_a_L111_R2_001.fastq.gz"), "wb") as ff:
-        with open(
-                "/home/cridenour/PycharmProjects/q2-pan-classifier/test_data/ENTV_05Mar2022/trimmed_sequences/ENTV87Skyview20220228_S29_L001_R2_output.fastq", "rb") as fastq_file:
-            ff.writelines(fastq_file)
-    with open(Path(output_directory.path).joinpath("MANIFEST"), "w") as ff:
-        ff.write(','.join(['sample-id', 'filename', 'direction\n']))
-        ff.write(','.join(['ENTV87Skyview20220228', '/home/cridenour/PycharmProjects/q2-pan-classifier/test_data/ENTV_05Mar2022/trimmed_sequences/ENTV87Skyview20220228_S29_L001_R1_output.fastq', 'forward\n']))
-        ff.write(','.join(['ENTV87Skyview20220228', '/home/cridenour/PycharmProjects/q2-pan-classifier/test_data/ENTV_05Mar2022/trimmed_sequences/ENTV87Skyview20220228_S29_L001_R2_output.fastq', 'reverse\n']))
+    output_dir = SingleLanePerSamplePairedEndFastqDirFmt()
 
-    with open(Path(output_directory.path).joinpath("metadata.yml"), "w") as ff:
-        ff.write(','.join(['sample-id']))
-    return output_directory
+    sequence_directory = os.path.abspath(sequence_directory)
+    temp_dir = tempfile.TemporaryDirectory()
+
+    manifest_file_path, names = _generate_manifest_file_(sequence_directory, "/home/cridenour/PycharmProjects/q2-pan-classifier/test_data/")
+
+    # manifest_file_path = "/home/cridenour/PycharmProjects/q2-pan-classifier/test_data/MANIFEST"
+    #
+    reads = qiime2.Artifact.import_data(type='SampleData[PairedEndSequencesWithQuality]',
+                                        view=manifest_file_path,
+                                        view_type='PairedEndFastqManifestPhred33V2')
+    reads.export_data(output_dir.path)
+    return output_dir
